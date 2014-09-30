b3266db46404c66e2043229238ff4fdcfbfad16a
Oliver4242 <oliver.duerr@gmail.com>
2014-09-30 15:43:16 +0200

    COMMENTED OUt executed code in packge

--- R/Testing.R
+++ R/Testing.R
@@ -3,13 +3,16 @@ if (FALSE) {
   createAB("~/tmp/book.Rmd")
 }
 
-setwd("/Users/oli/Dropbox/__ZHAW/WaST3/__Alle_HS14/__Ich__Wast3.HS14/")
-mergeRMD(dir = "Woche1/", title = "Arbeitsblatt 1", 
-         files = c("/Users/oli/Documents/workspace/wast/wast3/ARBEITSBLATT_01/aufgabe01/Arbeiten_mit_W-Dichten_Call-Center.Rmd",
-                   "/Users/oli/Documents/workspace/wast/wast3/ARBEITSBLATT_01/aufgabe02//Erwartungswert.Rmd",
-                   "/Users/oli/Documents/workspace/wast/wast3/ARBEITSBLATT_01/aufgabe03/Auswaehlen_und_Anpassen_von_Verteilungen.Rmd",
-                   "/Users/oli/Documents/workspace/wast/wast3/ARBEITSBLATT_01/aufgabe04/Berechnung_von_Erwartungswert_und_Varianz.Rmd"),
-         printpaths = TRUE,
-         mergedFileName = "ab1.Rmd"
-         )
-createAB(infile = "/Users/oli/Dropbox/__ZHAW/WaST3/__Alle_HS14/__Ich__Wast3.HS14/Woche1/ab1.Rmd")
+if (FALSE) {
+  setwd("/Users/oli/Dropbox/__ZHAW/WaST3/__Alle_HS14/__Ich__Wast3.HS14/")
+  mergeRMD(dir = "Woche1/", title = "Arbeitsblatt 1", 
+           files = c("/Users/oli/Documents/workspace/wast/wast3/ARBEITSBLATT_01/aufgabe01/Arbeiten_mit_W-Dichten_Call-Center.Rmd",
+                     "/Users/oli/Documents/workspace/wast/wast3/ARBEITSBLATT_01/aufgabe02//Erwartungswert.Rmd",
+                     "/Users/oli/Documents/workspace/wast/wast3/ARBEITSBLATT_01/aufgabe03/Auswaehlen_und_Anpassen_von_Verteilungen.Rmd",
+                     "/Users/oli/Documents/workspace/wast/wast3/ARBEITSBLATT_01/aufgabe04/Berechnung_von_Erwartungswert_und_Varianz.Rmd"),
+           printpaths = FALSE,
+           mergedFileName = "ab1.Rmd"
+           )
+  createAB(infile = "/Users/oli/Dropbox/__ZHAW/WaST3/__Alle_HS14/__Ich__Wast3.HS14/Woche1/ab1.Rmd")
+  rmdFiles <- list.files(path = "/Users/oli/Documents/workspace/wast/wast3/", pattern = "*.Rmd", recursive = TRUE)
+}
