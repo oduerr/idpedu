101ee24ed1939850ab0687720aac8c81d0704dde
hofc <hofc@zhaw.ch>
2017-01-07 16:37:59 +0100

    korrektur

--- R/CreateArbeitsblattMitBild.R
+++ R/CreateArbeitsblattMitBild.R
@@ -48,7 +48,7 @@ createAB <- function(infile, wast1 = FALSE, wastNum=-1) {
       before_body_img = system.file("rmarkdown/templates/aufgabe/resources/before_body_img_qs_asp_spc.tex", package = "idpedu")
     } else if(wastNum == 12){
       before_body_img = system.file("rmarkdown/templates/aufgabe/resources/before_body_img_qs_asp_spc_pruefung.tex", package = "idpedu")
-      }
+    }
   }
   header_nolsg =  system.file("rmarkdown/templates/aufgabe/resources/header_nolsg.tex", package = "idpedu")
   template <-  system.file(
