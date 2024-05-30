6e198f1a92ddd3756e4e50dae22e8eae5d718e6b
Oliver Dürr <oliver.duerr@gmail.com>
2024-05-30 17:33:57 +0200

    Added Buildignore

--- R/CreateAB2.R
+++ R/CreateAB2.R
@@ -55,17 +55,12 @@ remove_yaml_header <- function(lines) {
 #' @export
 #' 
 #' @examples:
-#' 
 #' tasks = c(
 #'  "https://raw.githubusercontent.com/oduerr/idpedu/master/demo/exercise1.qmd",
 #'  "https://raw.githubusercontent.com/oduerr/idpedu/master/demo/exercise2.qmd"
 #' ) # list of tasks (can also be a single task)
-#'
-#'
 #'title = "Week 1 (Simple Stuff)" # Title of the worksheet
 #'fname='week1' # Name of the file(s) which are produces
-#'
-# Stays the same for all worksheets for a given course
 #'header_file = "da.qmd" # To change / add go into repository inst/extdata
 #' create_workbook(tasks=tasks, title = title, fname=fname,  header_file=header_file)
 #' 
